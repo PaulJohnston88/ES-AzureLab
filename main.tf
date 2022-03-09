b6b857072cd1ae828521dd484c7a6f8f1d6b1341
PaulJohnston88 <paul.johnston@microsoft.com>
2022-03-09 14:59:37 +0000

    lots of changes after a lot of time

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ module "enterprise_scale" {
   root_name      = var.root_name
   library_path   = "${path.root}/lib"
 
-  deploy_core_landing_zones    = var.deploy_core_landing_zones
-  deploy_management_resources  = var.deploy_management_resources
-  subscription_id_management   = "b7190f8a-860a-428c-a303-7231b1eb2f60"
-  deploy_connectivity_resources = var.deploy_connectivity_resources
+  deploy_core_landing_zones        = var.deploy_core_landing_zones
+  deploy_management_resources      = var.deploy_management_resources
+  subscription_id_management       = "b7190f8a-860a-428c-a303-7231b1eb2f60"
+  deploy_connectivity_resources    = var.deploy_connectivity_resources
   configure_connectivity_resources = local.configure_connectivity_resources
-  subscription_id_connectivity = "e0583bb4-bb6c-44b8-9b39-72b0d4a1a6eb"
-  subscription_id_identity = "d161faf2-87a1-4b24-9425-7262dc2d83f0"
+  subscription_id_connectivity     = "e0583bb4-bb6c-44b8-9b39-72b0d4a1a6eb"
+  subscription_id_identity         = "d161faf2-87a1-4b24-9425-7262dc2d83f0"
 
   custom_landing_zones = {
     "${var.root_id}-online" = {
